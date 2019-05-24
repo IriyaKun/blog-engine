83698f5fa8b68264effd44368371a38297d2a49d
ilyagomel <quietbreasts@outlook.com>
2019-05-24 22:04:26 +0300

    refactor db context

--- BlogEngine.DLL/BlogEngineDbContext.cs
+++ BlogEngine.DLL/BlogEngineDbContext.cs
@@ -10,10 +10,31 @@ namespace BlogEngine.DLL
         public DbSet<User> Users { get; set; }
         public DbSet<Blog> Blogs { get; set; }
 
+        private IConfiguration _configuration;
+
+        public BlogEngineDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //TODO
-            optionsBuilder.UseMySQL("server=localhost;UserId=root;Password=bangarang12;database=blogEngineDb;");
+            switch(_configuration.dbProvider)
+            {
+                case "SQL_SERVER": 
+                {
+                    
+                    break;
+                }
+                case "MY_SQL": 
+                {
+                        optionsBuilder.UseMySQL("server=localhost;UserId=root;Password=bangarang12;database=blogEngineDb;");
+                        break;
+                }
+                default: { break; };
+            }
+
         }
     }
 }
---
+++ BlogEngine.DLL/IConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace BlogEngine.DLL
+{
+    public interface IConfiguration
+    {
+        string dbProvider { get; set; }
+        string connectionString { get; set; }
+    }
+}
